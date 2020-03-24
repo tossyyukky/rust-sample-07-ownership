01a1de3c8a4313604a0f3b2e1e5be0250703cfce
Toshiyuki Ihara <toshiyuki.ihara@noin.tv>
2020-03-25 00:20:29 +0900

    借用

--- src/ch07_02_move_semantics/mod.rs
+++ src/ch07_02_move_semantics/mod.rs
@@ -2,6 +2,16 @@
 use super::ch07_01_value_scop::Parent;
 use super::ch07_01_value_scop::Child;
 
+// Parentへの不変の参照を引数にとる
+fn f1(p: &Parent) {
+    println!("p: {:?}", p);
+}
+
+// Parentへの可変の参照を引数にとる
+fn f2(p: &mut Parent) {
+    p.0 *= -1;
+}
+
 pub fn main() {
     let mut p1 = Parent(1, Child(11), Child(12));
 
@@ -13,4 +23,9 @@ pub fn main() {
 
     p1 = Parent(2, Child(21), Child(22));
     println!("p1: {:?}", p1); // p1は別の値の所有権を持つのでアクセスできる
+
+    let mut p3 = Parent(3, Child(31), Child(32));
+    f1(&p3); // f1には所有権をムーブせず、不変の参照を渡す
+    f2(&mut p3); // f2には所有権をムーブせず、可変の参照を渡す
+    println!("p3: {:?}", p3);
 }
